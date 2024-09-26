81928065986ce77087376734cd76f97cedc81126
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-26 15:25:51 +0400

    user create nqavber done

--- src/Components/ui/Header/Navbar.tsx
+++ src/Components/ui/Header/Navbar.tsx
@@ -7,8 +7,6 @@ const Navbar = () => {
   const [menuButton, setMenuButton] = useState(false);
   const [theme, setTheme] = useState(false);
 
-  console.log({ theme });
-
   useEffect(() => {
     if (theme) {
       document.documentElement.classList.add("dark");
@@ -20,12 +18,7 @@ const Navbar = () => {
   }, [theme]);
 
   const handleChange = (value: string) => {
-    if (value === "Dark") {
-      setTheme(true);
-    } else {
-      setTheme(false);
-    }
-    console.log(`selected ${value}`);
+    setTheme(value === "Dark");
   };
 
   const options = [
@@ -33,7 +26,7 @@ const Navbar = () => {
       value: "Light",
       label: (
         <span>
-          <SunOutlined className="text-slate-500 bg-white  " />{" "}
+          <SunOutlined className="text-slate-500 bg-white" />{" "}
           <span className="me-2">Light Mode</span>
         </span>
       ),
@@ -42,7 +35,7 @@ const Navbar = () => {
       value: "Dark",
       label: (
         <span>
-          <MoonOutlined className="text-slate-500 bg-white  " />{" "}
+          <MoonOutlined className="text-slate-500 bg-white" />{" "}
           <span className="me-2">Dark Mode</span>
         </span>
       ),
@@ -51,59 +44,39 @@ const Navbar = () => {
 
   return (
     <div className="dark:bg-veryDarkBlue">
-      <nav className="relative container mx-auto p-6 ">
+      <nav className="relative container mx-auto p-6">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-20">
-            {/* <!-- Logo --> */}
             <div className="flex justify-center items-center gap-2">
-              <img className="size-8" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHgiisJIuJ_CUfcsffeseKZ54iK9MYwgYdsw&s' alt="" />
+              <img
+                className="size-8"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHgiisJIuJ_CUfcsffeseKZ54iK9MYwgYdsw&s"
+                alt="Logo"
+              />
               <p className="text-xl font-bold uppercase text-third-color dark:text-fourth-color">
-                {" "}
                 <span className="text-secondary-color">S</span>
                 <span className="text-primary-color">F</span> Platform
               </p>
             </div>
-            {/* <!-- Left Menu --> */}
             <div className="hidden text-lg uppercase space-x-8 font-bold lg:flex">
-              <NavLink
-                to="/about-us"
-                className="text-grayishViolet hover:text-strongCyan"
-              >
+              <NavLink to="/about-us" className="text-grayishViolet hover:text-strongCyan">
                 About Us
               </NavLink>
-              <NavLink
-                to="/contact-us"
-                className="text-grayishViolet hover:text-strongCyan"
-              >
+              <NavLink to="/contact-us" className="text-grayishViolet hover:text-strongCyan">
                 Contact us
               </NavLink>
-              <NavLink
-                to="/facility-listing"
-                className="text-grayishViolet hover:text-strongCyan"
-              >
+              <NavLink to="/facility-listing" className="text-grayishViolet hover:text-strongCyan">
                 Facility Listing
               </NavLink>
-              <NavLink
-                to="/booking"
-                className="text-grayishViolet hover:text-strongCyan"
-              >
+              <NavLink to="/booking" className="text-grayishViolet hover:text-strongCyan">
                 Booking
               </NavLink>
-              <NavLink to='/login'className="w-full text-center uppercase" > 
-              Login
-            </NavLink>
-            <NavLink to='/register'className="w-full text-center uppercase" > 
-              Register
-            </NavLink>
-            
+              <NavLink to='/login' className="text-center uppercase">Login</NavLink>
+              <NavLink to='/register' className="text-center uppercase">Register</NavLink>
             </div>
           </div>
-
-          {/* <!-- Right Buttons Menu --> */}
           <div className="hidden items-center space-x-4 font-bold text-grayishViolet lg:flex">
-            <div className="hover:text-varyDarkViolet custom-muted-button">
-              Login
-            </div>
+            <div className="hover:text-varyDarkViolet custom-muted-button">Login</div>
             <div>
               <Avatar
                 style={{
@@ -118,13 +91,10 @@ const Navbar = () => {
             </div>
           </div>
 
-          {/* <!--  Hamburger Menu --> */}
           <button
             id="menu-btn"
             type="button"
-            className={`${
-              !menuButton ? "open" : "block"
-            } hamburger lg:hidden focus:outline-none`}
+            className={`${!menuButton ? "open" : "block"} hamburger lg:hidden focus:outline-none`}
             onClick={() => setMenuButton(!menuButton)}
           >
             <span className="hamburger-top"></span>
@@ -133,12 +103,9 @@ const Navbar = () => {
           </button>
         </div>
 
-        {/* <!--  Mobile Menu --> */}
         <div
           id="menu"
-          className={`absolute p-6 rounded-lg bg-darkViolet left-6 right-6 top-20 z-100 lg:hidden ${
-            menuButton && "hidden"
-          }`}
+          className={`absolute p-6 rounded-lg bg-darkViolet left-6 right-6 top-20 z-100 lg:hidden ${menuButton && "hidden"}`}
         >
           <div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
             <NavLink to="/about-us" className="w-full text-center uppercase">
@@ -147,26 +114,18 @@ const Navbar = () => {
             <NavLink to="/contact-us" className="w-full text-center uppercase">
               Contact us
             </NavLink>
-            <NavLink
-              to="/facility-listing"
-              className="w-full text-center uppercase"
-            >
+            <NavLink to="/facility-listing" className="w-full text-center uppercase">
               Facility Listing
             </NavLink>
             <NavLink to="/booking" className="w-full text-center uppercase">
               Booking
             </NavLink>
-           <NavLink to='/login'className="w-full text-center uppercase" > 
-              Login
-            </NavLink>
-            <NavLink to='/regiter'className="w-full text-center uppercase" > 
-              Register
-            </NavLink>
-            
+            <NavLink to='/login' className="w-full text-center uppercase">Login</NavLink>
+            <NavLink to='/register' className="w-full text-center uppercase">Register</NavLink>
 
             <Select
               className="w-full"
-              defaultValue="Switch Color Mode "
+              defaultValue="Switch Color Mode"
               style={{ width: "100%" }}
               onChange={handleChange}
               options={options}
